0c77fc5d2eb82c6279e0ec8a34e775be12813a0b
אביחי סוריה <soria@g.jct.ac.il>
2020-10-14 16:06:45 +0300

    Initial code + Welcome6877

---
+++ dotNet5781_00_6877_2459/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_6877_2459
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome6877();
+            Welcome2459();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome2459();
+        private static void Welcome6877()
+        {
+            Console.Write("Enter your name: ");
+            string a = Console.ReadLine();
+            Console.WriteLine(a + ", welcome to my first console application");
+        }
+    }
+}
