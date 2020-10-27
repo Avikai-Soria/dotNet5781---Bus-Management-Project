c5d4c5b3cbbe5b0ed484d9484c59690413d45dde
אביחי סוריה <soria@g.jct.ac.il>
2020-10-27 23:13:15 +0200

    Created Bus and its constructor

---
+++ dotNet5781_01_6877_2459/Bus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_6877_2459
+{
+    class Bus
+    {
+        string id;
+        DateTime creationDate;
+        int overallMileage; // kilometraj
+        int fuel;
+        int currentMileage; 
+        public Bus(string idg, DateTime creationDateg, int overallMileageg, int fuelg , int currentMileageg)
+        {
+            id = idg;
+            creationDate = creationDateg;
+            overallMileage = overallMileageg;
+            fuel = fuelg;
+            currentMileage = currentMileageg;
+        }
+    }
+}
