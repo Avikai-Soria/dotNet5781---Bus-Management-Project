84680b9aa495c620d4e1e979ae14645b9382e5ec
אביחי סוריה <soria@g.jct.ac.il>
2020-10-29 11:59:53 +0200

    Pretty much done, need testing though

--- dotNet5781_01_6877_2459/Bus.cs
+++ dotNet5781_01_6877_2459/Bus.cs
@@ -10,23 +10,61 @@ namespace dotNet5781_01_6877_2459
     {
         string id;
         DateTime creationDate;
+        DateTime lastMaintenance;
         int overallMileage; // kilometraj
         int fuel;
-        int currentMileage; 
-        public Bus(string idg, DateTime creationDateg, int overallMileageg, int fuelg , int currentMileageg)
+        int currentMileage;
+        public string Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public Bus(string idg, DateTime creationDateg, int overallMileageg, int fuelg, int currentMileageg)
         {
             id = idg;
             creationDate = creationDateg;
+            lastMaintenance = creationDateg;
             overallMileage = overallMileageg;
             fuel = fuelg;
             currentMileage = currentMileageg;
         }
-        public override String ToString ()
+        public override String ToString()
         {
             String to_return = "Bus id: " + id + "\n" + "Activation date: " + creationDate + "\n" + "The bus's overall mileage is: "
-                + overallMileage + "\n" + "Current fuel is: " + fuel + "\n" + "The bus's current mileage is: " + currentMileage + "\n" +"\n";
+                + overallMileage + "\n" + "Current fuel is: " + fuel + "\n" + "The bus's current mileage is: " + currentMileage + "\n" + "\n";
             return (to_return);
         }
-        // funcs to make: 
+        public bool IsQualified()
+        {
+            if (currentMileage > 20000)                             // The bus have passed the allowed millage
+                return false;
+            if ((DateTime.Now - lastMaintenance).TotalDays > 365)   // It's been a year since the last maintaenance
+                return false;
+            return true;
+        }
+        public bool IsCapable(int travel)
+        {
+            if (fuel - travel < 0)                 // Not enough fuel for the travel
+                return false;
+            if (currentMileage + travel > 20000)   // The bus will pass the allowed millage during the travel.
+                return false;
+            return true;
+        }
+        public void PerformTravel(int travel)
+        {
+            overallMileage += travel;
+            currentMileage += travel;
+            fuel -= travel;
+        }
+        public void PerformRefueling()
+        {
+            fuel = 1200;
+        }
+        public void PerformMaintenance()
+        {
+            currentMileage = 0;
+            lastMaintenance = DateTime.Now;
+        }
     }
 }
--- dotNet5781_01_6877_2459/Program.cs
+++ dotNet5781_01_6877_2459/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Odbc;
+using System.IO;
 using System.IO.Pipes;
 using System.Linq;
 using System.Text;
@@ -10,10 +11,25 @@ namespace dotNet5781_01_6877_2459
 {
     class Program
     {
+        static string fix_id(string id)
+        {
+            if(id.Length==7)
+            {
+                id = id.Substring(0, 2) + '-' + id.Substring(2, 3) + '-' + id.Substring(5, 2);
+            }
+            else
+            {
+                id = id.Substring(0, 3) + '-' + id.Substring(3, 2) + '-' + id.Substring(5, 3);
+            }
+            return id;
+        }
         static void Main(string[] args)
         {
             List<Bus> list_of_buses = new List<Bus>();
-            int choice;
+            int choice;                                         // Will serve the switch case
+            int sub_choice;                                     // Mostly for case 3
+            Random r = new Random(DateTime.Now.Millisecond); // r will serve us through the whole program
+            int testid;                                      // This int will be used to test the id digits
             Console.WriteLine("Welcome user! What action would you like to do today?");
             do
             {
@@ -42,27 +58,25 @@ namespace dotNet5781_01_6877_2459
                         if ((date.Year) < 2018)
                         {
                             Console.WriteLine("Please insert 7 digits of the license number."); // Assuming first digit can't be 0.
-                            int testid;
                             id = Console.ReadLine();
                             while (!Int32.TryParse(id, out testid) || ((testid < 1000000) || (testid > 9999999)))
                             {
                                 Console.WriteLine("Invalid input was entered, make sure you insert only 7 digits.");
                                 id = Console.ReadLine();
                             }
-                            id = id.Substring(0, 2) + '-' + id.Substring(2, 3) + '-' + id.Substring(5, 2);
+                            id = fix_id(id);
                             Console.WriteLine(id);
                         }
                         else
                         {
                             Console.WriteLine("Please insert 8 digits of the license number."); // Assuming first digit can't be 0.
-                            int testid;
                             id = Console.ReadLine();
                             while (!Int32.TryParse(id, out testid) || ((testid < 10000000) || (testid > 99999999)))
                             {
-                                Console.WriteLine("Invalid input was entered, make sure you insert only 7 digits.");
+                                Console.WriteLine("Invalid input was entered, make sure you insert only 8 digits.");
                                 id = Console.ReadLine();
                             }
-                            id = id.Substring(0, 3) + '-' + id.Substring(3, 2) + '-' + id.Substring(5, 3);
+                            id = fix_id(id);
                             Console.WriteLine(id);
                         }
                         int overallMileage;         // IMPORTANT
@@ -87,22 +101,92 @@ namespace dotNet5781_01_6877_2459
                         Console.WriteLine(current_bus);
                         list_of_buses.Add(current_bus);
                         break;
+
                     case 2:     // Select bus for travel
                         Console.WriteLine("Case 2");
+                        string search_id;
+                        Console.WriteLine("Please insert the license number of the bus you would like to use.");
+                        Console.WriteLine("Insert only 7 digits or 8 digits");
+                        search_id = Console.ReadLine();
+                        while (!Int32.TryParse(search_id, out testid) || ((testid < 1000000) || (testid > 99999999)))
+                        {
+                            Console.WriteLine("Invalid input was entered, make sure you insert only 7 or 8 digits.");
+                            search_id = Console.ReadLine();
+                        }
+                        search_id = fix_id(search_id);
+                        Bus current = null;
+                        foreach (Bus check in list_of_buses)
+                            if (check.Id == search_id)
+                                current = check;
+                        int travel_r;
+                        travel_r = r.Next(0, 1200);        // min drive will be 0 km, max would be 1200 km
+                        Console.WriteLine("The travel will last " + travel_r + " kilometers.");
+                        if (current == null)
+                        {
+                            Console.WriteLine("Bus's license number doesn't exist in the list.");
+                            break;
+                        }
+                        if (!current.IsQualified())
+                        {
+                            Console.WriteLine("The bus is not qualified for a travel.");
+                            break;
+                        }
+                        if(!current.IsCapable(travel_r))
+                        {
+                            Console.WriteLine("The bus is not capable of completing this travel safely.");
+                            break;
+                        }
+                        current.PerformTravel(travel_r);
+                        Console.WriteLine("The travel succeeded! The bus's current details are updated.");
                         break;
+
                     case 3:     // Maintain a bus
                         Console.WriteLine("Case 3");
+                        string to_fix_id;
+                        Console.WriteLine("Please insert the license number of the bus you would like to use.");
+                        Console.WriteLine("Insert only 7 digits or 8 digits");
+                        to_fix_id = Console.ReadLine();
+                        while (!Int32.TryParse(to_fix_id, out testid) || ((testid < 1000000) || (testid > 99999999)))
+                        {
+                            Console.WriteLine("Invalid input was entered, make sure you insert only 7 or 8 digits.");
+                            to_fix_id = Console.ReadLine();
+                        }
+                        to_fix_id = fix_id(to_fix_id);
+                        Bus current_to_fix = null;
+                        foreach (Bus check in list_of_buses)
+                            if (check.Id == to_fix_id)
+                                current_to_fix = check;
+                        if (current_to_fix == null)
+                        {
+                            Console.WriteLine("Bus's license number doesn't exist in the list.");
+                            break;
+                        }
+                        Console.WriteLine("The bus was found! What action would you like to perform?");
+                        Console.WriteLine("Insert 1 if you want to perform refueling.");
+                        Console.WriteLine("Insert 2 if you want to perform maintenance");
+                        while (!Int32.TryParse(Console.ReadLine(), out sub_choice) || ((choice < 1) || (choice > 2)))
+                        {
+                            Console.WriteLine("Invalid input was entered, please insert either 1 or 2.");
+                        }
+                        if(sub_choice==1)
+                        {
+                            current_to_fix.PerformRefueling();
+                            Console.WriteLine("The bus was refueled successfully");
+                        }
+                        else
+                        {
+                            current_to_fix.PerformMaintenance();
+                            Console.WriteLine("The bus was maintinced successfully");
+                        }
                         break;
                     case 4:     // Display mileage
                         Console.WriteLine("Case 4");
-                        foreach(Bus current in list_of_buses)
-                            Console.WriteLine(current);
+                        foreach(Bus bus_to_print in list_of_buses)
+                            Console.WriteLine(bus_to_print);
                         break;
                     case 5:     // End
                         Console.WriteLine("Case 5");
-                        break;
-                    default:
-                        Console.WriteLine("Invalid input was entered, please select again");
+                        Console.WriteLine("End of the program. Cya!");
                         break;
                 }
             } while (choice != 5);
